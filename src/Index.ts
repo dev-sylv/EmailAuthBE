28ca72576ab448a08aa4745558b03c251d37c053
nicsylvia <nicsylvia15f@gmail.com>
2023-04-13 17:09:27 -0700

    saved server

---
+++ src/Index.ts
@@ -0,0 +1,32 @@
+import express, { Application, Request, Response } from "express";
+// import { DBCONNECTION } from "./Config/Database";
+
+// import { EnvironmentVariables } from "./Config/EnvironmentVariables";
+// import { AppConfig } from "./MainApp";
+
+// const port = EnvironmentVariables.PORT;
+const port = 3445;
+
+const app: Application = express();
+// AppConfig(app);
+// DBCONNECTION();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "API READY FOR DIRTY ONLINE PROJECT",
+  });
+});
+const server = app.listen(port, () => {
+  console.log("");
+  console.log("Server is up and running on port", port);
+});
+
+// To protect my server from crashing when users do what they are not supposed to do
+process.on("uncaughtException", (error: Error) => {
+  process.exit(1);
+});
+process.on("unhandledRejection", (res) => {
+  server.close(() => {
+    process.exit(1);
+  });
+});

b7a1f60137834321bd6dac0f6e9acb2f063c2e0a
nicsylvia <nicsylvia15f@gmail.com>
2023-04-13 23:07:55 -0700

    create company logic done

--- src/Controller/CompanyController.ts
+++ src/Controller/CompanyController.ts
@@ -61,3 +61,33 @@ export const deleteCompany = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const createCompany = async (req: Request, res: Response) => {
+  try {
+    const { email, password, name } = req.body;
+
+    const token = crypto.randomBytes(64).toString("hex");
+    const OTP = crypto.randomBytes(2).toString("hex");
+    const RCNumber = crypto.randomBytes(4).toString("hex");
+
+    const companies = await companyModel.create({
+      email,
+      password,
+      name,
+      token,
+      RCNumber,
+      OTP,
+    });
+
+    companyEmailVerification(companies).then(() => {
+      console.log("email sent");
+    });
+
+    res.status(200).json({
+      message: "companies",
+      data: companies,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};

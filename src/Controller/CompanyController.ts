8842bf6f2c165a962d0718a7ec7b25561670e09f
nicsylvia <nicsylvia15f@gmail.com>
2023-04-14 11:52:28 -0700

    saved company login

--- src/Controller/CompanyController.ts
+++ src/Controller/CompanyController.ts
@@ -132,33 +132,31 @@ export const VerifyCompany = async (req: Request, res: Response) => {
 export const SignInCompany = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const companies = await companyModel.findById(id);
+    const { email, password } = req.body;
+    const companies = await companyModel.findOne({ email });
 
-    if (companies!.token !== "") {
-      const company = await companyModel.findByIdAndUpdate(
-        id,
-        {
-          token: "",
-          verified: true,
-        },
-        {
-          new: true,
+    if (companies) {
+      if (password === companies?.password) {
+        if (companies?.verified && companies?.token === "") {
+          res.status(200).json({
+            message: "Welcome back",
+            data: companies,
+          });
+        } else {
+          res.status(400).json({
+            message: "Account not verified",
+          });
         }
-      );
-      res.status(200).json({
-        message: "companies",
-        data: company,
-      });
+      } else {
+        res.status(400).json({
+          message: "User Email Error",
+        });
+      }
     } else {
       res.status(400).json({
-        message: "An error occured in verification",
+        message: "User Password Error",
       });
     }
-
-    res.status(200).json({
-      message: "companies",
-      data: companies,
-    });
   } catch (error) {
     console.log(error);
   }
